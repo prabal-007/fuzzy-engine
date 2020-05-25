46f5ba6996203c59f1c66fb59f72cc25f2d30cf8
Starkk <42775517+prabal-007@users.noreply.github.com>
2020-05-25 20:34:45 +0530

    Update Email_app.java

--- Email_app.java
+++ Email_app.java
@@ -5,9 +5,9 @@ public class Email_app {
 	public static void main(String[] args) {
 		Email em1 = new Email("Tony","Stark");
 		
-		// em1.setAlternameEmail(stark456@marvel.com);
+		// em1.setAlternameEmail("stark456@marvel.com";
 		// em1.setmailboxCapacity(20);
-		// em1.changePassword(pass12345);
+		// em1.changePassword("ass12345";
 		System.out.println(em1.showInfo());		
 	}
 
